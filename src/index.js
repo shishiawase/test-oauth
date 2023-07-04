19b263ba43f53e07a140bdb7fac49be6381936ce
xtave <xtave@yandex-team.ru>
2023-07-04 14:20:02 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,25 +1,12 @@
-// Авторизуем пользователя, используя инфу о нем.
-const authorize = ({
-  default_avatar_id: defaultAvatarId,
-  display_name: displayName,
-}) => {
-  const avatarHtml = `<div class="avatar" style="background-image:url('https://avatars.mds.yandex.net/get-yapic/${defaultAvatarId}/islands-middle')"></div>`;
-  const nameHtml = `<div class="name">${displayName}</div>`;
-
-  document.getElementById("auth").innerHTML = `${avatarHtml}${nameHtml}`;
-};
-
-// Делаем запрос за инфой о пользователе.
-const fetchYandexData = (token) =>
-  fetch(`https://login.yandex.ru/info?format=json&oauth_token=${token}`).then(
-    (res) => res.json()
-  );
+const getUnixTimeStamp = (shift = 0) => Number((new Date() / 1000 + shift) | 0);
 
 window.onload = () => {
-  document.getElementById("suggest").onclick = () => {
-    // TODO suggest
-  };
   document.getElementById("button").onclick = () => {
-    // TODO button
+    const date = new Date();
+    const timestamp = Number(date);
+
+    document.getElementById(
+      "logs"
+    ).innerHTML = `Date: ${date}<br />Timestamp: ${timestamp}`;
   };
 };
