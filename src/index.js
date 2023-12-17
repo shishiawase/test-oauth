30323390e357ebfdf97c2f460ee5fbce87f9a076
shishiawase <53081951+shishiawase@users.noreply.github.com>
2023-12-17 08:37:01 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ window.onload = () => {
   document.getElementById("button").onclick = () => {
     window.YaAuthSuggest.init(
       {
-        client_id: "c46f0c53093440c39f12eff95a9f2f93",
+        client_id: "5d752d1cc538431ab8e367cba7fc77a4",
         response_type: "token",
-        redirect_uri: "https://examplesite.com/suggest/token",
+        redirect_uri: "https://test-oauth-eight.vercel.app/token.html",
       },
-      "https://examplesite.com",
+      "hhttps://test-oauth-eight.vercel.app",
       {
         view: "button",
         parentId: "buttonContainer",
@@ -18,7 +18,13 @@ window.onload = () => {
       }
     )
       .then(({ handler }) => handler())
-      .then((data) => console.log("Сообщение с токеном", data))
-      .catch((error) => console.log("Обработка ошибки", error));
+      .then(async (data) => {
+        const result = await fetchYandexData(data.access_token);
+
+        authorize(result);
+
+        console.log(result, data);
+      })
+      .catch((error) => console.log("Что-то пошло не так: ", error));
   };
 };
